a4bde8b66592857bebc28b860f9e6b2c0b5a9bc8
PythonGuython <corrigank.matt@gmail.com>
2021-08-22 16:03:01 -0400

    detection test

--- multitest.cpp
+++ multitest.cpp
@@ -1,4 +1,17 @@
+#include <arv.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 int main()
 {
+    ArvCamera *camera;
+	ArvBuffer *buffer;
+
+    camera = arv_camera_new(NULL, NULL);
+
+    if (ARV_IS_CAMERA (camera)) {
+        printf("camera found\n")
+    }
+
     return 0;
 }
