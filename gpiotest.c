2440d9ce503be329c61a31119a7c964e21cb0a38
PythonGuython <corrigank.matt@gmail.com>
2021-09-24 19:33:05 -0400

    set pin high for 10 s

--- gpiotest.c
+++ gpiotest.c
@@ -5,12 +5,17 @@
 int main(){
 
     if (wiringPiSetup() == -1) {
-    printf("wiringPi setup failed/n");
-    return -1;}
+        printf("wiringPi setup failed/n");
+        return -1;
+    }
     
     else{
     printf("wiring setup successful");
     }
+
+    digitalWrite(LedPin, HIGH);
+    delay(10000);
+    digitalWrite(LedPin, LOW);
     
     
     return 0;
